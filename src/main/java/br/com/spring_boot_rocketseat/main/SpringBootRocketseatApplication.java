faa6e57ecc2b2b99765ed6a4011dfcdd7967e5a3
xsalles <xs.salles@gmail.com>
2025-09-09 14:36:29 -0300

    feat: created a get route with a path param

--- src/main/java/br/com/spring_boot_rocketseat/main/SpringBootRocketseatApplication.java
+++ src/main/java/br/com/spring_boot_rocketseat/main/SpringBootRocketseatApplication.java
@@ -2,8 +2,10 @@ package br.com.spring_boot_rocketseat.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.spring_boot_rocketseat")
 public class SpringBootRocketseatApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/spring_boot_rocketseat/main/controllers/FirstController.java
+++ src/main/java/br/com/spring_boot_rocketseat/main/controllers/FirstController.java
@@ -3,6 +3,7 @@ package br.com.spring_boot_rocketseat.main.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -12,6 +13,11 @@ public class FirstController {
     @GetMapping("/method")
     public String getMethodName() {
         return "Deu certo";
+    }
+
+    @GetMapping("/method/{id}")
+    public String getPathParam(@PathVariable Integer id) {
+        return "O parâmetro é: " + id;
     }
     
 }
