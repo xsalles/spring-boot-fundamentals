8779c5943ffc6d71e3786a1eed65c410c5021a9e
xsalles <xs.salles@gmail.com>
2025-09-08 22:22:32 -0300

    feat: started the project in spring boot

---
+++ src/main/java/br/com/spring_boot_rocketseat/main/SpringBootRocketseatApplication.java
@@ -0,0 +1,13 @@
+package br.com.spring_boot_rocketseat.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRocketseatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRocketseatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/spring_boot_rocketseat/main/SpringBootRocketseatApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring_boot_rocketseat.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRocketseatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
