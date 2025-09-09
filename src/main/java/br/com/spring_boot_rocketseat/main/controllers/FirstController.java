ebfab5ee81a26f2d9e103a0a9f51a3da5971d58a
xsalles <xs.salles@gmail.com>
2025-09-09 14:57:34 -0300

    feat: created a get route with a body param

--- src/main/java/br/com/spring_boot_rocketseat/main/controllers/FirstController.java
+++ src/main/java/br/com/spring_boot_rocketseat/main/controllers/FirstController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -23,6 +24,11 @@ public class FirstController {
 
     @GetMapping("/queryParam")
     public String getQueryParam(@RequestParam Integer id) {
+       return "O parâmetro é: " + id;
+    }
+
+    @GetMapping("/bodyParam")
+    public String getBodyParam(@RequestBody String id) {
        return "O parâmetro é: " + id;
     }
 }
